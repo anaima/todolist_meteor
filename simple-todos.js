afaf9bf509f5fc5163d1c2007a0fad0c7a23a3ef
anaima <Namilya@gmail.com>
2016-02-22 22:07:10 +0100

    add a checkbox on simple-todos.html/ add a event handler for checkbox on simple-todos.js and add helpers to body template

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,18 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
 
+    hideCompleted: function () {
+
+      return Session.get("hideCompleted");
   // Show newest tasks at the top
   return Tasks.find({}, {sort: {createdAt: -1}});
   return Tasks.find({});
@@ -43,11 +54,13 @@ if (Meteor.isClient) {
       // Clear form
 
       event.target.text.value = "";
-
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
 
   });
-  
+
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
