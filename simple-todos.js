d9188c6e68176080b28bceb2d43b49cf827a49f5
anaima <Namilya@gmail.com>
2016-02-22 22:20:32 +0100

    add incompleteCount helper (a focntion) to body on simple-todos.js and display it on simple-todos.html --> to show completed tasks in opacity

--- simple-todos.js
+++ simple-todos.js
@@ -17,6 +17,10 @@ if (Meteor.isClient) {
     hideCompleted: function () {
 
       return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+
   // Show newest tasks at the top
   return Tasks.find({}, {sort: {createdAt: -1}});
   return Tasks.find({});
